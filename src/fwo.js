4203f44bcca9b6dede3c7608057714d2c22a6744
cat <cat@nxt.ru>
2019-10-08 22:53:42 +0300

    Small fix

--- src/fwo.js
+++ src/fwo.js
@@ -7,5 +7,5 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 if (!process.env.BOT_TOKEN) console.log('Problem in BOT_TOKEN')
 
 bot.on('text', ({ replyWithHTML }) => replyWithHTML('<b>Hello</b>'))
-bot.on('message',(ctx) => ctx.replay('ff')};
+bot.on('message',(ctx) => ctx.replay('ff'));
 bot.launch();
