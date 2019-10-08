865aebe62eedf93c26da0da7892b884a12bc128b
cat <info@fw-online.ru>
2019-10-08 16:55:26 +0300

    add express for tests

--- src/fwo.js
+++ src/fwo.js
@@ -2,5 +2,14 @@
  * Startup Module
  */
 const Telegraf = require('telegraf');
+const express = require('express');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+bot.on('text', ({ replyWithHTML }) => replyWithHTML('<b>Hello</b>'))
+
+const app = express()
+app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bot.webhookCallback('/secret-path'))
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
