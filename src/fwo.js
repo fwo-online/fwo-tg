a8052fcebe40ce64648670f173f3da30ebd47318
cat <info@fw-online.ru>
2019-10-08 18:51:41 +0300

    code fix

--- src/fwo.js
+++ src/fwo.js
@@ -1,11 +1,11 @@
 /**
  * Startup Module
  */
-const Telegraf = require('telegraf');
-const express = require('express');
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const Telegraf = require('telegraf')
+const express = require('express')
+const bot = new Telegraf(process.env.BOT_TOKEN)
 
-if (! process.env.BOT_TOKEN) console.log('Problem in BOT_TOKEN')
+if (!process.env.BOT_TOKEN) console.log('Problem in BOT_TOKEN')
 
 bot.on('text', ({ replyWithHTML }) => replyWithHTML('<b>Hello</b>'))
 
@@ -14,4 +14,4 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.use(bot.webhookCallback('/secret-path'))
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
-});
+})
