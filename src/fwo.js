aa2a5a0a9a0251060c28f2a30d98bf3646d3af55
cat <info@fw-online.ru>
2019-10-08 17:39:53 +0300

    debug for heroku

--- src/fwo.js
+++ src/fwo.js
@@ -5,6 +5,8 @@ const Telegraf = require('telegraf');
 const express = require('express');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+if (! process.env.BOT_TOKEN) console.log('Problem in BOT_TOKEN')
+
 bot.on('text', ({ replyWithHTML }) => replyWithHTML('<b>Hello</b>'))
 
 const app = express()
@@ -12,4 +14,4 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.use(bot.webhookCallback('/secret-path'))
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
-})
+});
