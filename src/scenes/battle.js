5c3285cc91aac6f95d29f24d44e8803e45610d74
cat <info@fw-online.ru>
2019-10-23 17:07:02 +0300

    wip mm start

--- src/scenes/battle.js
+++ src/scenes/battle.js
@@ -5,22 +5,26 @@
 const Scene = require('telegraf/scenes/base');
 const Markup = require('telegraf/markup');
 const channelHelper = require('../helpers/channelHelper');
-// const Char = require('../arena/CharacterService');
+const Char = require('../arena/CharacterService');
+const arena = require('../arena');
 
 const battleScene = new Scene('battleScene');
-battleScene.enter(({ reply, session }) => {
+battleScene.enter(({ reply }) => {
   reply(
-    `Кнопки: charId > ${session.character.nickname}`,
+    'Кнопки',
     Markup.inlineKeyboard([
-      Markup.callbackButton('Атака', 'Атака'),
-      Markup.callbackButton('Защита', 'Защита'),
-      Markup.callbackButton('Магии', 'Магии'),
+      Markup.callbackButton('Искать приключений на ...', 'search'),
     ]).resize().extra(),
   );
 });
 
-battleScene.action('Атака', ({ reply }) => {
-  reply('Мессага в личный чат');
+battleScene.action('search', async ({ reply }) => {
+  // eslint-disable-next-line no-undef,no-underscore-dangle
+  const chatObject = await Char.loading(session.character._id);
+  // eslint-disable-next-line no-console
+  console.log(JSON.stringify(global.arena));
+  arena.mm.push(chatObject);
+  await reply('Мессага в личный чат');
   channelHelper.broadcast('Мессага в общий чат');
 });
 
