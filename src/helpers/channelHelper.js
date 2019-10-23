2a80a4e3524e76aa02763db4d65751fd46e044f0
ekirpichyov <e.kirpichyov@ya.ru>
2019-10-23 16:03:42 +0300

    add jsdoc

--- src/helpers/channelHelper.js
+++ src/helpers/channelHelper.js
@@ -1,7 +1,14 @@
+/**
+ * Помощник для отправки сообщений в общий чат
+ */
+
 const chatId = '-331233606';
 
 module.exports = {
   bot: null,
+  /**
+   * @param {string} data - текст отправляемого сообщения
+   */
   broadcast(data) {
     this.bot.telegram.sendMessage(chatId, data);
   },
