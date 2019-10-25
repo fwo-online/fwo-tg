2b47531ef0d647aace382783a802d5043ee4931c
ekirpichyov <e.kirpichyov@ya.ru>
2019-10-25 17:08:48 +0300

    фикс arena.players

--- src/arena/CharacterService.js
+++ src/arena/CharacterService.js
@@ -135,6 +135,7 @@ class Char {
     // изменяем прототип
     // eslint-disable-next-line no-proto
     p.__proto__ = Object.create(this.prototype);
+    if (!arena.players) arena.players = {};
     arena.players[charId] = p;
     // eslint-disable-next-line no-console
     console.log(JSON.stringify(arena.players));
