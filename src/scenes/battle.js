2846dda6ec2d33e4771d8b7d9850b82c4cc72d4e
ekirpichyov <47474371+ekirpichyov@users.noreply.github.com>
2019-11-06 22:39:31 +0300

    fix battle orders

--- src/scenes/battle.js
+++ src/scenes/battle.js
@@ -7,10 +7,7 @@ const Markup = require('telegraf/markup');
 const channelHelper = require('../helpers/channelHelper');
 const Char = require('../arena/CharacterService');
 const arena = require('../arena');
-const Orders = require('../arena/OrderService');
 const GameService = require('../arena/GameService');
-
-const orders = new Orders();
 
 const battleScene = new Scene('battleScene');
 battleScene.enter(({ reply }) => {
@@ -50,8 +47,10 @@ battleScene.action(/action(?=_)/, async ({ editMessageText, session, match }) =>
 
 battleScene.action(/\w*_\w*_\w*/, async ({ editMessageText, session, match }) => {
   const [action, target, nick] = match.input.split('_');
+  const gameId = global.arena.players[session.character._id].mm;
+  const Game = global.arena.games[gameId];
   // eslint-disable-next-line no-underscore-dangle
-  orders.orderAction(session.character._id, target, action, 100);
+  Game.orders.orderAction(session.character._id, target, action, 100);
   editMessageText(
     `Заказан ${action} на игрока ${nick}`,
   );
