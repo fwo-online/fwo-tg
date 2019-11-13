0f6b914f96f12f1eabcce3a63e9c6d354fd2e6b0
cat <info@fw-online.ru>
2019-11-13 17:41:47 +0300

    Небольшие косметические доработки в статистике

--- src/scenes/profile.js
+++ src/scenes/profile.js
@@ -97,6 +97,7 @@ profile.action(/increase(?=_)/, ({ session, editMessageText, match }) => {
 });
 
 profile.action('confirm', async ({ session, update, editMessageText }) => {
+  // @todo сюда нужно будет предусмотреть проверки на корректность сохраняемых данных
   await loginHelper.saveHarks(update.callback_query.from.id, session.character);
   // eslint-disable-next-line no-param-reassign
   allHarks.forEach((hark) => delete session.character.harks[`${hark}Temp`]);
