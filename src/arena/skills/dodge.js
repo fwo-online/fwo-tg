12b2bd2c3b76592851c90bed08f241b192cbefca
cat <info@fw-online.ru>
2019-11-18 13:17:07 +0300

    add dodge skill

---
+++ src/arena/skills/dodge.js
@@ -0,0 +1,28 @@
+const Skill = require('../Constuructors/SkillConstructor');
+/**
+ * Увертка
+ */
+const dodge = new Skill({
+  name: 'dodge',
+  desc: 'Шанс увернуться от одной или нескольких атак(только против колющего, режущего, рубящего, метательного, оглушающего оружия)',
+  cost: [10, 12, 14, 16, 18, 20],
+  proc: 20,
+  baseExp: 50,
+  costType: 'en',
+  lvl: 1,
+  orderType: 'self',
+  aoeType: 'target',
+  chance: [75, 80, 85, 90, 95, 99],
+  effect: [1.2, 1.25, 1.3, 1.35, 1.4, 1.45],
+  msg: (nick, exp) => `${nick} использовал [Увертка] [📖 +${exp}]`,
+  profList: ['l'],
+});
+/**
+ * Логика работы скила
+ * @param {Object} initiator Обьект кастера
+ */
+dodge.run = (initiator = this.params.initiator) => {
+  const initiatorSkillLvl = initiator.skills[this.name];
+  initiator.flags.isDodging = this.effect[initiatorSkillLvl - 1] * initiator.stats.val('dex');
+};
+module.exports = dodge;
