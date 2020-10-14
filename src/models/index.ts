4a92de76966921c1d9bf078c34af6558bc07d9c5
cat <info@fw-online.ru>
2020-10-14 11:45:44 +0300

    WIP docker-compose.yml

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 mongoose.connect(
-  'mongodb+srv://test:tRSpcaCAiBYGKEy5@cluster0.xtp0l.mongodb.net/Cluster0?retryWrites=true&w=majority',
+  'mongodb+srv://root:fworootpassword@db/fwogame?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useFindAndModify: false,
