f52331ac6daa10535402b6506ec600f7a1d86986
cat <cat@nxt.ru>
2021-02-19 18:30:08 +0300

    fix db

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 mongoose.connect(
-  'mongodb://root:fworootpassword@localhost:27017/fwo?retryWrites=true&w=majority&authSource=admin',
+  'mongodb://root:fworootpassword@db:27017/fwo?retryWrites=true&w=majority&authSource=admin',
   {
     useNewUrlParser: true,
     useFindAndModify: false,
