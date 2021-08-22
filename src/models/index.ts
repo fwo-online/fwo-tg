7987b6563fec5dd25e5c917804ad2b77be709862
cat <cat@nxt.ru>
2021-08-22 15:47:33 +0300

    add defaul for mongo

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 // MONGO - полный mongo uri:
 // "mongodb://user:password@db:27017/fwo?retryWrites=true&w=majority&authSource=admin 
 mongoose.connect(
-  process.env.MONGO,
+  process.env.MONGO ?? 'mongodb://user:password@db:27017/fwo?retryWrites=true&w=majority&authSource=admin',
   {
     useNewUrlParser: true,
     useFindAndModify: false,
