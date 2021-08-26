c37d5c5e1bffb0cac2517d14e793e873edfed10b
cat <cat@nxt.ru>
2021-08-26 22:13:03 +0300

    обратная совместимость для mongo uri

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 // MONGO - полный mongo uri:
 // "mongodb://user:password@db:27017/fwo?retryWrites=true&w=majority&authSource=admin 
 mongoose.connect(
-  process.env.MONGO ?? 'mongodb://user:password@db:27017/fwo?retryWrites=true&w=majority&authSource=admin',
+  process.env.MONGO ?? 'mongodb://root:fworootpassword@db:27017/fwo?retryWrites=true&w=majority&authSource=admin',
   {
     useNewUrlParser: true,
     useFindAndModify: false,
