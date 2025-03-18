88e4670898af924de5880f6e95421970dbd52518
kyvg <e.kirpichyov@ya.ru>
2025-03-18 08:35:17 +0200

    add logger

--- server/server/index.ts
+++ server/server/index.ts
@@ -5,11 +5,13 @@ import { magic } from './magic';
 import { skill } from './skill';
 import { shop } from './shop';
 import { cors } from 'hono/cors';
+import { logger } from 'hono/logger';
 import { isString } from 'es-toolkit';
 
 const origin = [process.env.APP_URL].filter(isString);
 
 export const app = new Hono()
+  .use(logger())
   .use(cors({ origin }))
   .route('/character', character)
   .route('/inventory', inventory)
