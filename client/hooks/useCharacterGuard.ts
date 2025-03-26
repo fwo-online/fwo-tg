f43d0de8e733e294b29b66ff91149fa52579f42f
Evgeny Kirpichyov <47474371+kyvg@users.noreply.github.com>
2025-03-26 22:32:10 +0200

    #359 Fix rerenders after game end (#395)
    Co-authored-by: kyvg <e.kirpichyov@ya.ru>

--- client/hooks/useCharacterGuard.ts
+++ client/hooks/useCharacterGuard.ts
@@ -1,21 +1,28 @@
+import { useCharacter } from '@/contexts/character';
 import { useWebSocket } from '@/contexts/webSocket';
 import { useMountEffect } from '@/hooks/useMountEffect';
 import { useUpdateCharacter } from '@/hooks/useUpdateCharacter';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 
 export const useCharacterGuard = () => {
   const socket = useWebSocket();
+  const { character, setCharacter } = useCharacter();
   const { updateCharacter } = useUpdateCharacter();
+
+  const handleGameEnd = useCallback(() => {
+    setCharacter(() => Object.assign(structuredClone(character), { game: undefined }));
+    updateCharacter();
+  }, [character, updateCharacter, setCharacter]);
 
   useMountEffect(() => {
     socket.io.on('reconnect', updateCharacter);
   });
 
   useEffect(() => {
-    socket.on('game:end', updateCharacter);
+    socket.on('game:end', handleGameEnd);
 
     return () => {
       socket.off('game:end');
     };
-  }, [socket, updateCharacter]);
+  }, [socket, handleGameEnd]);
 };
