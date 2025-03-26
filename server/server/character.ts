b4f498c888c0003a38c2188ad3e15b69935bd65a
kyvg <e.kirpichyov@ya.ru>
2025-03-26 20:24:04 +0200

    #373 Валидация имён

--- server/server/character.ts
+++ server/server/character.ts
@@ -6,26 +6,40 @@ import { profsData } from '@/data/profs';
 import { characterAttributesSchema, createCharacterSchema } from '@fwo/shared';
 import { characterMiddleware, userMiddleware } from './middlewares';
 import { withValidation } from '@/server/utils/withValidation';
+import { flatten } from 'valibot';
+import { HTTPException } from 'hono/http-exception';
 
 export const character = new Hono()
   .use(userMiddleware)
-  .post('/', vValidator('json', createCharacterSchema), async (c) => {
-    const createCharacterDto = await c.req.valid('json');
-    const user = c.get('user');
-    await withValidation(
-      createCharacter({
-        owner: user.id.toString(),
-        nickname: createCharacterDto.name,
-        prof: createCharacterDto.class,
-        harks: profsData[createCharacterDto.class].hark,
-        magics: profsData[createCharacterDto.class].mag,
-        sex: 'm',
-      }),
-    );
+  .post(
+    '/',
+    vValidator('json', createCharacterSchema, (result) => {
+      if (!result.success) {
+        const message = Object.values(flatten(result.issues).nested ?? {})
+          .flat()
+          .join(', ');
 
-    const character = await CharacterService.getCharacter(user.id.toString());
-    return c.json(character.toObject(), 200);
-  })
+        throw new HTTPException(400, { message });
+      }
+    }),
+    async (c) => {
+      const createCharacterDto = await c.req.valid('json');
+      const user = c.get('user');
+      await withValidation(
+        createCharacter({
+          owner: user.id.toString(),
+          nickname: createCharacterDto.name,
+          prof: createCharacterDto.class,
+          harks: profsData[createCharacterDto.class].hark,
+          magics: profsData[createCharacterDto.class].mag,
+          sex: 'm',
+        }),
+      );
+
+      const character = await CharacterService.getCharacter(user.id.toString());
+      return c.json(character.toObject(), 200);
+    },
+  )
   .use(characterMiddleware)
   .get('/', async (c) => {
     const character = c.get('character');
