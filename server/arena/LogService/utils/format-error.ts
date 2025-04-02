6a5ccf7421a8d0d79e2ddc65287a54d8bf3bb38f
kyvg <e.kirpichyov@ya.ru>
2025-04-02 17:39:00 +0300

    feat: update magic change fail message

--- server/arena/LogService/utils/format-error.ts
+++ server/arena/LogService/utils/format-error.ts
@@ -31,7 +31,7 @@ export function formatError(msgObj: FailArgs): string {
         en: '',
       },
       CHANCE_FAIL: {
-        ru: `${bold(initiator.nick)} пытался сотворить ${italic(action)}, но у него не вышло`,
+        ru: `${bold(initiator.nick)} пытался сотворить ${italic(action)} на ${bold(target.nick)}, но он защитился`,
         en: '',
       },
       GOD_FAIL: {
