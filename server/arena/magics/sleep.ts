46641c632f16633005dedfd8da57a6cbe4fd46ea
Evgeny Kirpichyov <47474371+kyvg@users.noreply.github.com>
2025-04-15 09:43:50 +0300

    Fix sleep (#440)
    * fix: clear sleep debuff after hit
    
    * add comment

--- server/arena/magics/sleep.ts
+++ server/arena/magics/sleep.ts
@@ -21,7 +21,7 @@ class Sleep extends LongMagic implements Affect {
       magType: 'bad',
       chance: ['1d80', '1d90', '1d100'],
       profList: ['m'],
-      effect: ['1d1','1d1','1d1'],
+      effect: ['1d1', '1d1', '1d1'],
     });
   }
 
@@ -36,7 +36,13 @@ class Sleep extends LongMagic implements Affect {
   }
 
   preAffect: Affect['preAffect'] = ({ params: { initiator: target, game } }): undefined => {
-    if (target.flags.isSleeping.length && !target.flags.isHited) {
+    if (target.flags.isHited) {
+      /** @todo сделать метод в конструкторе для очистки длительных бафов */
+      game.longActions.sleep = game.longActions.sleep?.filter((item) => item.target !== target.id);
+      target.flags.isSleeping = [];
+    }
+
+    if (target.flags.isSleeping.length) {
       throw new CastError(
         target.flags.isSleeping.map(({ initiator }) =>
           this.getSuccessResult({
