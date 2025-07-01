db7fff85c9a4be9c31a54929433cc5115330a17e
kyvg <47474371+kyvg@users.noreply.github.com>
2025-07-01 17:03:36 +0300

    fix chance

--- server/arena/Constuructors/MagicConstructor.ts
+++ server/arena/Constuructors/MagicConstructor.ts
@@ -170,7 +170,7 @@ export abstract class Magic extends AffectableAction {
       const defence = target.stats.val('magic.defence');
       const ratio = attack / defence;
 
-      result *= 1 - Math.exp(-0.75 * ratio);
+      result *= 1 - Math.exp(-1 * ratio);
     }
     console.debug(
       `${this.name} cast chance:: ${result * initiator.proc} (${result}), chance ${chance}, ratio (dmg): ${initiator.stats.val('magic.attack') / target.stats.val('magic.defence')} initiator:: ${initiator.nick}, target:: ${target.nick}`,
