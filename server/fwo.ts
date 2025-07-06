3692003fc614cc350c9ef982c6c03416c9015a38
kyvg <47474371+kyvg@users.noreply.github.com>
2025-07-06 21:14:40 +0300

    init api on bd connection

--- server/fwo.ts
+++ server/fwo.ts
@@ -1,15 +1,15 @@
-import { app } from '@/server';
-import { connect } from './models';
-import { ItemModel } from './models/item';
-import { registerAffects } from './utils/registerAffects';
-import { registerGlobals } from './utils/registerGlobals';
 import { serve } from '@hono/node-server';
-import { Server } from 'socket.io';
-import { middleware, onConnection, onCreate } from '@/server/ws';
 import { isString } from 'es-toolkit';
-import { initGameChannel } from './helpers/channelHelper';
-import { ItemSetModel } from './models/item-set';
+import { Server } from 'socket.io';
 import { initBot } from '@/bot';
+import { initGameChannel } from '@/helpers/channelHelper';
+import { connect } from '@/models';
+import { ItemModel } from '@/models/item';
+import { ItemSetModel } from '@/models/item-set';
+import { app } from '@/server';
+import { middleware, onConnection, onCreate } from '@/server/ws';
+import { registerAffects } from '@/utils/registerAffects';
+import { registerGlobals } from '@/utils/registerGlobals';
 
 // DB connection
 void connect(async () => {
@@ -18,25 +18,26 @@ void connect(async () => {
   await ItemSetModel.load();
 
   initGameChannel();
+
+  const server = serve({
+    fetch: app.fetch,
+    port: 3000,
+  });
+
+  const io = new Server(server, {
+    connectionStateRecovery: {},
+    pingInterval: 10000,
+    cors: { origin: [process.env.APP_URL].filter(isString) },
+  });
+
+  onCreate(io);
+  io.use(middleware(io));
+  io.on('connection', (socket) => {
+    onConnection(io, socket);
+  });
+
   await initBot();
 });
 
 registerGlobals();
 registerAffects();
-
-const server = serve({
-  fetch: app.fetch,
-  port: 3000,
-});
-
-const io = new Server(server, {
-  connectionStateRecovery: {},
-  pingInterval: 10000,
-  cors: { origin: [process.env.APP_URL].filter(isString) },
-});
-
-onCreate(io);
-io.use(middleware(io));
-io.on('connection', (socket) => {
-  onConnection(io, socket);
-});
