16bb39400e4ab07d33a65782b62d4bfad33c85c8
kyvg <47474371+kyvg@users.noreply.github.com>
2026-08-15 23:33:35 +0200

    fix build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
@@ -15,7 +15,9 @@ export default defineConfig({
         runes: ({ filename }) =>
           filename.split(/[/\\]/).includes('node_modules') ? undefined : true,
       },
-      adapter: adapter(),
+      adapter: adapter({
+        fallback: '200.html', // may differ from host to host
+      }),
     }),
   ],
   resolve: {
